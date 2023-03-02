e7236cf2f81423dc9c5cf471e47931c00a4a3028
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-02 14:59:47 +0700

    Create random_quote_gennarator.py

---
+++ random_quote_gennarator.py
@@ -0,0 +1,11 @@
+import random
+
+quote = [
+    "Lorem ipsum dolor sit amet, con",
+    "Lorem ipsum dolor sit amet, con et inter lobortis",
+    "Lorem ipsum dolor sit amet, con et inter lob Fortune",
+    ]
+
+result = random.choice(quote)
+
+print(result)
