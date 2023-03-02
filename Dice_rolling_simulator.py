e5aa44deab76d90743d4f3ec05b88c2f90cf323d
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-02 14:59:36 +0700

    Create Dice_rolling_simulator.py

---
+++ Dice_rolling_simulator.py
@@ -0,0 +1,16 @@
+import random
+
+"""
+result = random.randint(1, 6)
+
+print("\n- - - - - - - - -\n",result,"\n- - - - - - - - -")
+"""
+
+while True:
+    x = input("Enter Any Key : ")
+    if x != None:
+        result = random.randint(1, 6)
+        print("\n- - - - - - - - -\n",result,"\n- - - - - - - - -")
+    else:
+        result = random.randint(1, 6)
+        print("\n- - - - - - - - -\n",result,"\n- - - - - - - - -")
